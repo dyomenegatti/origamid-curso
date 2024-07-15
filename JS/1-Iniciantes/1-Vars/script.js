acda69c94fbfcd917e8afe9e2407c1e3dd223951
dyovanamenegattibefore <dyovana.menegatti@before.com.br>
2024-07-15 20:59:09 -0300

    Aula de variaveis

---
+++ JS/1-Iniciantes/1-Vars/script.js
@@ -0,0 +1,23 @@
+// Declarar uma variável com o seu nome
+var nome = 'Dyovana';
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+let idade = 27;
+console.log(idade);
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+let comidaFavorita;
+console.log(comidaFavorita);
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Hambúrguer';
+console.log(comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+let var1, 
+    var2, 
+    var3, 
+    var4, 
+    var5;
+console.log(var1, var2, var3, var4, var5);
